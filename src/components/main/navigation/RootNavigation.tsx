43e3aec998c400f7a65f3d71c43d5c8b63e1a2db
trankAvz <duytk41@gmail.com>
2023-10-09 14:43:21 +0700

    Update Navigation
    thấy và đã fix lỗi
    ERROR  Warning: Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render.
        in PreventRemoveProvider (created by NavigationContent)
        in NavigationContent
        in Unknown (created by BottomTabNavigator)
        in BottomTabNavigator (created by BottomTab)
        in BottomTab (created by AppNavitgation)

--- src/components/main/navigation/RootNavigation.tsx
+++ src/components/main/navigation/RootNavigation.tsx
@@ -2,31 +2,65 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import UserNavigation from './UserNavigation';
 import AppNavitgation from './AppNavitgation';
 import AccountStackNavigation from './AccountStackNavigation';
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+
+
+export enum ParamsScreenEnum {
+  // Login
+  Splash = 'Splash',
+  Wk = 'Wk',
+  Signin = 'Signin',
+  Signup = 'Signup',
+  Phone = 'Phone',
+  ConfirmPhone = 'ConfirmPhone',
+
+  EnterAddress = 'EnterAddress',
+  ForgotPass = 'ForgotPass',
+  ResetPass = 'ResetPass',
+
+  //Tab
+  Home = 'Home',
+  Search = 'Search',
+  Orders = 'Orders',
+  AccountSetting = 'AccountSetting',
+
+  //Navigation
+  AppNavigation = 'AppNavigation',
+  UserNavigation = 'UserNavigation',
+  AccountStackNavigation = 'AccountStackNavigation',
+
+}
 
 export type ParamList = {
-    // Login
-    Splash: undefined;
-    Wk: undefined;
-    Signin: undefined;
-    Signup: undefined;
-    Phone: undefined;
-    ConfirmPhone: undefined;
+  // Login
+  Splash: undefined;
+  Wk: undefined;
+  Signin: undefined;
+  Signup: undefined;
+  Phone: undefined;
+  ConfirmPhone: undefined;
 
-    EnterAddress: undefined;
-    ForgotPass: undefined;
-    ResetPass: undefined;
+  EnterAddress: undefined;
+  ForgotPass: undefined;
+  ResetPass: undefined;
 
-    //Tab
-    Home: undefined;
-    Search: undefined;
-    Orders: undefined;
-    AccountSetting: undefined;
+  //Tab
+  Home: undefined;
+  Search: undefined;
+  Orders: undefined;
+  AccountSetting: undefined;
+
+  //Navigation
+  AppNavigation: undefined;
+  UserNavigation: undefined;
+  AccountStackNavigation: undefined;
+
 
 }
 
@@ -34,11 +68,26 @@ const RootNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      // initialRouteName='AppNavitgation'
-      screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name='AppNavigation' component={AppNavitgation}/> */}
-        <Stack.Screen name='UserNavigation' component={UserNavigation}/>
+        initialRouteName='UserNavigation'
+        // backGround 
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyle: { backgroundColor: '#000' },
+          transitionSpec: {
+            open: { animation: 'timing', config: { duration: 300 } },
+            close: { animation: 'timing', config: { duration: 300 } },
+          },
+          headerStyle: {
+            backgroundColor: 'transparent',
+          },
+        }}
+        >
+        <Stack.Screen name='AppNavigation' component={AppNavitgation} />
+        <Stack.Screen name='UserNavigation' component={UserNavigation} />
       </Stack.Navigator>
+      {/* <AppNavitgation/> */}
     </NavigationContainer>
   )
 }
--- src/components/main/screens/inforStack/Log/Signup.tsx
+++ src/components/main/screens/inforStack/Log/Signup.tsx
@@ -12,25 +12,41 @@ import {
 import TopBar from '../../../../customs/Others/TopBar';
 import B1 from '../../../../customs/Button/B1';
 import Form1 from '../../../../customs/Form/Form1';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { ParamList, ParamsScreenEnum } from '../../../navigation/RootNavigation';
+
 
 const Signup = () => {
   const greeting1: string = 'Enter your Name, Email and Password for sign up. ';
   const greeting2: string = 'Already have account?';
   const term: string =
     'By Signing up you agree to our Terms Conditions & Privacy Policy.';
+
+  const navigation = useNavigation<NavigationProp<ParamList>>();
+  const NavigateTo = (name: keyof ParamList) => {
+    navigation.navigate(name);
+  }
+
+  const Back = () => {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor={'white'} />
 
-      <TopBar left name="Sign Up" />
+      <TopBar left name="Sign Up"
+        onBack={() => { Back() }}
+      />
 
       <View style={[styles.container1]}>
-        <Text style={[font.h1Title, {color: Colors.Main}]}>Create Account</Text>
+        <Text style={[font.h1Title, { color: Colors.Main }]}>Create Account</Text>
 
         <Text
-          style={[font.body, {width: 275, marginBottom: 34, marginTop: 20}]}>
+          style={[font.body, { width: 275, marginBottom: 34, marginTop: 20 }]}>
           {greeting1}
-          <Text style={{color: Colors.active}}>{greeting2}</Text>
+          <Text style={{ color: Colors.active }}
+            onPress={() => { NavigateTo(ParamsScreenEnum.Signin) }}>{greeting2}</Text>
         </Text>
 
         <Form1
@@ -48,12 +64,12 @@ const Signup = () => {
         />
         <Form1 showIcon password placeholder="Password" />
 
-        <B1 title="Sign up" style={{marginVertical: 20}} />
+        <B1 title="Sign up" style={{ marginVertical: 20 }} />
 
         <Text
           style={[
             font.caption,
-            {textAlign: 'center', width: 285, alignSelf: 'center'},
+            { textAlign: 'center', width: 285, alignSelf: 'center' },
           ]}>
           {term}
         </Text>
